eb5a1594839d92e7ff170495480eea0ff732ddc6
milan115 <milanlm798@gmail.com>
2021-11-13 01:30:11 +0600

    Updates

---
+++ precache-manifest.353a4b19d108ce671ec476debf3c1fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1c08cf12f1cab896edabd100a8938a",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "8406b74c6ec647828a5e",
+    "url": "/pizza/static/css/main.23c436d8.chunk.css"
+  },
+  {
+    "revision": "b811773e7ef5755e62eb",
+    "url": "/pizza/static/js/2.a2338763.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pizza/static/js/2.a2338763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8406b74c6ec647828a5e",
+    "url": "/pizza/static/js/main.35593e19.chunk.js"
+  },
+  {
+    "revision": "397f97b2c9960635976f",
+    "url": "/pizza/static/js/runtime-main.ce209449.js"
+  }
+]);
